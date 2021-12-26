b658b3b7dfc0186901b804f01c90b114e83a2686
parvg555 <parvg555@gmail.com>
2021-12-27 00:03:57 +0530

    started socket io application

--- SocketIO TUT/client/src/App.js
+++ SocketIO TUT/client/src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import { useState,useEffect } from 'react';
+import io from 'socket.io-client'
+
+let socket;
+const CONNECTION_PORT = 'localhost:8000/'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>React app</h1>
+
+  	const [loggedIn, setloggedIn] = useState(false);
+	
+
+
+return (
+	<div className="App">
+      	{!loggedIn? (
+          	<div className="logIn">
+				<div className="inputs">
+					<input type="text" placeholder='name' />
+					<input type="text" placeholder='room code' />
+				</div>
+            	<button>Enter Chat</button>
+          	</div>
+        
+      	):(
+        	<h1>You are logged in</h1> 
+      	)}
     </div>
   );
 }
--- SocketIO TUT/server/server.js
+++ SocketIO TUT/server/server.js
@@ -1,15 +1,34 @@
 import express from 'express'
 import Cors from 'cors'
 import mongoose from 'mongoose'
+import { Server, Socket } from 'socket.io';
 
 const app = express();
 const port = process.env.PORT || 8001;
 
+
+app.use(express.json());
 app.use(Cors());
 
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Listening on localhost:${port}`);
 })
 
+io = Socket(server)
 
+io.on('connection', (socket)=>{
+    console.log(socket.id);
+
+    socket.on('join_room', (data) => {
+        socket.join(data);
+        console.log('USER JOINED ROOM: ' + data);
+    })
+
+    
+
+
+    socket.on('disconnect', ()=>{
+        console.log('user disconnected');
+    });
+})
