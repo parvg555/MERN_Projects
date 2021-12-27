af4b6d1b826ab0e81e50955c2b9daa1d018150e7
parvg555 <parvg555@gmail.com>
2021-12-28 00:03:12 +0530

    learned socketio

--- SocketIO TUT/client/src/App.js
+++ SocketIO TUT/client/src/App.js
@@ -3,30 +3,107 @@ import { useState,useEffect } from 'react';
 import io from 'socket.io-client'
 
 let socket;
-const CONNECTION_PORT = 'localhost:8000/'
+const CONNECTION_PORT = "http://localhost:8001/"
 
 function App() {
 
+	// before logging in
   	const [loggedIn, setloggedIn] = useState(false);
+	const [room, setRoom] = useState("");
+	const [username, setUsername] = useState('');
+
+
+	//after logging in
+	const [message, setMessage] = useState("");
+	const [messageList, setMessageList] = useState([]);
+
+	useEffect(() => {
+		socket = io(CONNECTION_PORT);
+	},[CONNECTION_PORT]);
+
+	useEffect(() => {
+		socket.on("receive_message", (data) => {
+			// console.log(data);
+			setMessageList([...messageList,data]);
+		});
+	})
+
+	const connectToRoom = () => {
+		setloggedIn(true);
+		socket.emit('join_room', room);
+	}
+
+	const sendMessage = async () => {
+		let messageContent = {
+			room: room,
+			content: {
+				author: username,
+				message:message
+			}
+		}
+		await socket.emit("send_message", messageContent);
+		setMessageList([...messageList,messageContent.content]);
+		setMessage("");
+	}
+
+	return (
+		<div className="App">
+	      	{!loggedIn? (
+	          	<div className="logIn">
+					<div className="inputs">
+						<input 
+							type="text" 
+							placeholder='name'
+							onChange={(e) => {
+								setUsername(e.target.value);
+							}} 
+						/>
+						<input 
+							type="text" 
+							placeholder='room code' 
+							onChange={(e) => {
+								setRoom(e.target.value);
+							}}
+						/>
+					</div>
+	            	<button
+						onClick={connectToRoom}
+					>
+						Enter Chat
+					</button>
+	          	</div>
 	
-
-
-return (
-	<div className="App">
-      	{!loggedIn? (
-          	<div className="logIn">
-				<div className="inputs">
-					<input type="text" placeholder='name' />
-					<input type="text" placeholder='room code' />
+	      	):(
+	        	<div className="chatContainer">
+					<div className='messages'>
+						{
+							messageList.map((data) => {
+								return (
+									<h1>
+										{data.author}
+										{data.message}
+									</h1>
+								)
+							})
+						}
+					</div>
+					<div className='message-inputs'>
+						<input 
+							type="text" 
+							value={message}
+							placeholder='Message...'
+							onChange={(e) => {
+								setMessage(e.target.value);
+							}} 
+						/>
+						<button
+							onClick={sendMessage}
+						>Send</button>
+					</div>
 				</div>
-            	<button>Enter Chat</button>
-          	</div>
-        
-      	):(
-        	<h1>You are logged in</h1> 
-      	)}
-    </div>
-  );
+	      	)}
+	    </div>
+	);
 }
 
 export default App;
--- SocketIO TUT/server/server.js
+++ SocketIO TUT/server/server.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import Cors from 'cors'
 import mongoose from 'mongoose'
-import { Server, Socket } from 'socket.io';
+import {Server, Socket } from 'socket.io';
+import http from 'http';
 
 const app = express();
 const port = process.env.PORT || 8001;
@@ -10,25 +11,37 @@ const port = process.env.PORT || 8001;
 app.use(express.json());
 app.use(Cors());
 
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {
+    cors : {
+        origin:"http://localhost:3000",
+        methods: ["GET","POST"]
+    }
+});
 
-const server = app.listen(port, () => {
-    console.log(`Listening on localhost:${port}`);
+app.get("/",(req,res) => {
+    return res.send("hello ji");
 })
 
-io = Socket(server)
-
-io.on('connection', (socket)=>{
+io.on("connection", (socket) => {
     console.log(socket.id);
-
-    socket.on('join_room', (data) => {
+    socket.on("join_room" ,(data) => {
         socket.join(data);
-        console.log('USER JOINED ROOM: ' + data);
+        console.log("USER JOINED ROOM: " + data);
     })
 
-    
+    socket.on("send_message", (data) => {
+        console.log(data);
+        socket.to(data.room).emit("receive_message", data.content);
+    })
 
-
-    socket.on('disconnect', ()=>{
-        console.log('user disconnected');
-    });
+    socket.on("disconnect" , () => {
+        console.log("USER DISCONNECTED");
+    })
 })
+
+
+httpServer.listen(port, () => {
+    console.log(`listening to localhost:${port}`);
+})
+
